7ad3e2fe3a11b8de647da53eebb070ca0abcc980
eraldo6 <eraldograbovaj06@gmail.com>
2024-11-26 12:25:08 +0100

    updated queries

--- src/main/java/TPCDSBenchmark.java
+++ src/main/java/TPCDSBenchmark.java
@@ -23,29 +23,29 @@ public class TPCDSBenchmark {
     }
 
     private static void executeQuery(TableEnvironment tableEnv) {
-        String query = "WITH customer_total_return AS (" +
-                "SELECT sr_customer_sk AS ctr_customer_sk, " +
-                "       sr_store_sk AS ctr_store_sk, " +
-                "       SUM(sr_fee) AS ctr_total_return " +
-                "FROM store_returns, date_dim " +
-                "WHERE sr_returned_date_sk = d_date_sk " +
-                "  AND d_year = 2000 " +
-                "GROUP BY sr_customer_sk, sr_store_sk" +
-                ") " +
-                "SELECT c.c_customer_id " +
-                "FROM customer_total_return ctr1 " +
-                "JOIN store s ON s.s_store_sk = ctr1.ctr_store_sk " +
-                "JOIN customer c ON c.c_customer_sk = ctr1.ctr_customer_sk " +
-                "WHERE ctr1.ctr_total_return > ( " +
-                "    SELECT AVG(ctr_total_return) * 1.2 " +
-                "    FROM customer_total_return ctr2 " +
-                "    WHERE ctr1.ctr_store_sk = ctr2.ctr_store_sk" +
-                ") " +
-                "AND s.s_state = 'NM' " +
-                "ORDER BY c.c_customer_id " +
-                "LIMIT 100";
-
-        tableEnv.executeSql(query).print();
+//        String query = "WITH customer_total_return AS (" +
+//                "SELECT sr_customer_sk AS ctr_customer_sk, " +
+//                "       sr_store_sk AS ctr_store_sk, " +
+//                "       SUM(sr_fee) AS ctr_total_return " +
+//                "FROM store_returns, date_dim " +
+//                "WHERE sr_returned_date_sk = d_date_sk " +
+//                "  AND d_year = 2000 " +
+//                "GROUP BY sr_customer_sk, sr_store_sk" +
+//                ") " +
+//                "SELECT c.c_customer_id " +
+//                "FROM customer_total_return ctr1 " +
+//                "JOIN store s ON s.s_store_sk = ctr1.ctr_store_sk " +
+//                "JOIN customer c ON c.c_customer_sk = ctr1.ctr_customer_sk " +
+//                "WHERE ctr1.ctr_total_return > ( " +
+//                "    SELECT AVG(ctr_total_return) * 1.2 " +
+//                "    FROM customer_total_return ctr2 " +
+//                "    WHERE ctr1.ctr_store_sk = ctr2.ctr_store_sk" +
+//                ") " +
+//                "AND s.s_state = 'NM' " +
+//                "ORDER BY c.c_customer_id " +
+//                "LIMIT 100";
+//
+//        tableEnv.executeSql(query).print();
 
         // Test if tables are loaded correctly
         tableEnv.executeSql("SELECT COUNT(*) FROM store_returns").print();
